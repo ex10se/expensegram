f347c6e2a07fc242b39bc0ffa17cb73ffd3f2218
Mikhail Beliakov <Mikhail.Belyakov@x5.ru>
2023-02-13 12:28:27 +0500

    3.5 Настройка tox

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -1,3 +1,13 @@
 from .base import *
 
-DATABASES['default']['HOST'] = 'localhost'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': 'localhost',
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+        'CONN_MAX_AGE': None,
+    }
+}
